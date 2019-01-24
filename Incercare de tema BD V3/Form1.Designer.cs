40b5ca2bd0ab18769b30e7fc8413687270008905
andrei nfs <andreinfs@yahoo.com>
2019-01-25 01:41:50 +0200

    Innitial commit

--- Incercare de tema BD V3/Form1.Designer.cs
+++ Incercare de tema BD V3/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.StudentiDGV = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.StudentiDGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(528, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // StudentiDGV
+            // 
+            this.StudentiDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.StudentiDGV.Location = new System.Drawing.Point(24, 12);
+            this.StudentiDGV.Name = "StudentiDGV";
+            this.StudentiDGV.Size = new System.Drawing.Size(388, 236);
+            this.StudentiDGV.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StudentiDGV);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.StudentiDGV)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView StudentiDGV;
     }
 }
 
--- Incercare de tema BD V3/Form1.cs
+++ Incercare de tema BD V3/Form1.cs
@@ -7,14 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Incercare_de_tema_BD_V3
 {
     public partial class Form1 : Form
     {
+        String connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ELEVI;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            String sqlCommand = "SELECT * FROM STUDENTI";  
+            using (connection)
+            {
+                connection.Open();
+                SqlDataAdapter command = new SqlDataAdapter(sqlCommand, connection);
+
+                DataTable dt = new DataTable();
+                command.Fill(dt);
+                StudentiDGV.AutoGenerateColumns = true;
+                StudentiDGV.DataSource = dt;
+            }
         }
     }
 }
